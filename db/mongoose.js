131a4986a62d2685fc1c9a1d9573b717f438db86
unknown <chudasamaakshay45@gmail.com>
2020-02-09 19:55:23 +0530

    MONGODB URI changed

--- db/mongoose.js
+++ db/mongoose.js
@@ -8,9 +8,13 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
     console.log('Connected to MongoDB Server!');
   })
   .catch(e => {
+    console.log(e);
     console.log('Unable to Connect MongoDB Server! \n', e);
   });
 
 module.exports = {
   mongoose
 }
+// "MONGODB_URI": "mongodb+srv://Dysthymia:Dysthymia101@cluster0-8tmy8.mongodb.net/test?retryWrites=true&w=majority",
+// "MONGODB_URI": "mongodb://localhost:27017/DysthymiaApp",
+// mongodb://Dysthymia:Dysthymia101@cluster0-shard-00-00-8tmy8.mongodb.net:27017,cluster0-shard-00-01-8tmy8.mongodb.net:27017,cluster0-shard-00-02-8tmy8.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
